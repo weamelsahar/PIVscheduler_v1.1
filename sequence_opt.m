c4aaf9df03a6236f6ba55b4607ae06dd30db1f5f
Weam Elsahhar <weamelsahar@gmail.com>
2018-10-18 04:53:04 +0100

    1- added squence_opt for multi threading

---
+++ sequence_opt.m
@@ -0,0 +1,30 @@
+function [groups]=sequence_opt(Images_a,group_max_size)
+count=0;
+groups=[];
+for index=1:length(Images_a)
+    if index==1
+        count= 1;
+    else
+        currentindex=str2num(cell2mat(regexp(Images_a(index).name,'\d*','Match')));
+        previousindex=str2num(cell2mat(regexp(Images_a(index-1).name,'\d*','Match')));
+        
+        if ((currentindex - previousindex) ==1)                    % If the next image is in sequence example 837 - 838 and not crossing *998 - *999
+            count = count+1;                                       % Increment number of images in group
+            if (floor(currentindex/1000)~=floor(previousindex/1000))||(count>group_max_size) % updtae groups and exit 'if' the index crossed *999 or the maximum group size
+                groups=[groups , count-1];                         
+                count=1;
+
+            end
+            if index == length(Images_a)
+                groups=[groups , count];
+                count=0;
+            end
+        elseif count == 1                       %If next image is not in sequence and the number of images in the group is only =1 
+            groups=[groups , 1];                %Use 1 as a flg to add the next image because '.cas' can't configure 1 image only
+            count=1;
+        else
+            groups=[groups , count];
+            count=1;
+        end
+    end
+end
